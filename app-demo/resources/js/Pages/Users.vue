2756f4b2ed285ce716a74757f8a806961f806a15
ssouzafs <ssouza.s@hotmail.com>
2022-07-04 11:51:37 -0300

    Desfeita separação de filtro de pesquisa, pois não pude resolver um erro que apresentava

--- app-demo/resources/js/Pages/Users.vue
+++ app-demo/resources/js/Pages/Users.vue
@@ -10,14 +10,12 @@
       </h1>
       <div class="mt-6 w-full">
         <div class="mt-6 mb-3 flex justify-end">
-          <Filter @informSearch="(value) => search = value" :props="props" />
-
-          <!--          <input-->
-          <!--            type="search"-->
-          <!--            placeholder="Pesquisar..."-->
-          <!--            class="h-9 text-sm p-2 w-2/5 rounded-lg outline-none focus:ring focus:ring-indigo-200  border border-indigo-400"-->
-          <!--            v-model="search"-->
-          <!--          >-->
+          <input
+            type="search"
+            placeholder="Pesquisar..."
+            class="h-9 text-sm p-2 w-2/5 rounded-lg outline-none focus:ring focus:ring-indigo-200  border border-indigo-400"
+            v-model="search"
+          >
         </div>
         <ListUsers :users="users" />
       </div>
@@ -30,22 +28,18 @@
   import { Inertia } from "@inertiajs/inertia";
   import Paginator from "../Shareds/Paginator";
   import ListUsers from "../Shareds/ListUsers";
-  import Filter from "../Shareds/Filter";
-  //
-  const search = ref("");
 
   let props = defineProps({
     users: Object,
     filters: Object
   });
-  // const search = ref(props.filters.search);
+  const search = ref(props.filters.search);
+
   watch(search, (value) => {
     Inertia.get("/users", { search: value }, {
       preserveState: true,
       replace: true
     });
-
-    console.log(search)
   });
 </script>
 
